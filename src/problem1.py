2dcaa039ed71e9148854e1a7f515807a04987c25
adisethupathy <adisethupathy@gmail.com>
2018-03-20 21:22:02 -0400

    exam1problem1

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 1, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Adi Sethupathy.  March 2018.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   product_of_digits   function below.
+# done: 2.  READ the doc-string for the   product_of_digits   function below.
 #           Ask your instructor for help if you do not understand it.
 #
 #   Once you are confident that you understand the doc-string
@@ -196,7 +196,7 @@ def problem1a(a, b, threshold):
               returns 109 + 110 + 111 + 112 + 120 + 121 + 130,  which is 813
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # IMPORTANT NOTE
@@ -205,11 +205,19 @@ def problem1a(a, b, threshold):
     #     1-argument version of the RANGE statement to solve this problem.
     # -------------------------------------------------------------------------
 
+    sum = 0
+    for k in range((b - a) + 1):
+        if (product_of_digits(k+a)<threshold):
+            sum += k + a
+    return sum
+
+
+
 
 def run_test_problem1b():
     """ Tests the   problem1b   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement at least 4 tests of the problem1b function.
+    # done: 4. Implement at least 4 tests of the problem1b function.
     #   Note that you CANNOT use  problem1b(1)  or  problem1b(2)  as tests, per
     #   the specification below that says that the argument must be at least 3.
     # -------------------------------------------------------------------------
@@ -217,6 +225,30 @@ def run_test_problem1b():
     print('--------------------------------------------------')
     print('Testing the   problem1b   function:')
     print('--------------------------------------------------')
+
+    #test 1
+    expected = 682
+    actual = problem1b(8)
+    print('expected:', expected)
+    print('actual', actual)
+
+    # test 1
+    expected = 143
+    actual = problem1b(5)
+    print('expected:', expected)
+    print('actual', actual)
+
+    # test 1
+    expected = 276
+    actual = problem1b(6)
+    print('expected:', expected)
+    print('actual', actual)
+
+    # test 1
+    expected = 1386
+    actual = problem1b(10)
+    print('expected:', expected)
+    print('actual', actual)
 
 
 ###############################################################################
@@ -239,9 +271,12 @@ def problem1b(r):
            and the sum of the above numbers is 682
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # done: 5. Implement and test this function.
     #          See the IMPORTANT NOTE just before the DEF above.
     # -------------------------------------------------------------------------
+
+    squared = r ** 2
+    return problem1a(r, squared, r+1)
 
 
 # -----------------------------------------------------------------------------
